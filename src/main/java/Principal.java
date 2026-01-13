fc9363bdeaeaa00f9327574afe6e0fe6eb6b2055
Matías <matiasarce214@gmail.com>
2026-01-12 23:53:23 -0300

    Primera actualización

---
+++ src/main/java/Principal.java
@@ -0,0 +1,16 @@
+public class Principal {
+
+    public static void main(String[] args){
+        int numero = 6;
+        System.out.println("Número entero: "+numero);
+        double decimal = 7.5;
+        System.out.println("Número flotante: "+decimal);
+        byte edad = 35;
+        System.out.println("Entero corto (byte): "+edad);
+        char letra = 'a';
+        System.out.println("Caracter: "+letra);
+        boolean booleano = true;
+        System.out.println("Valor booleano: "+booleano);
+    }
+
+}
